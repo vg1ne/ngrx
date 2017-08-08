8932b54ef0f4560044ef1b78d6d13273685a2d2a
vgoreiko <vgoreiko@deloitte.com>
2017-08-08 16:08:03 +0300

    refactoring of actions

--- ngrxApp/src/app/_reducers/counter.ts
+++ ngrxApp/src/app/_reducers/counter.ts
@@ -1,40 +1,15 @@
 import {Action} from "@ngrx/store"
-
-export const INCREMENT = '[Counter] INCREMENT'
-export const DECREMENT = '[Counter] DECREMENT'
-export const RESET = '[Counter] RESET'
-export const INCREMENTBYARG = '[Counter] INCREMENTBYARG'
-
-export class IncrementAction implements Action{
-  readonly type = INCREMENT
-}
-export class DecrementAction implements Action{
-  readonly type = DECREMENT
-}
-export class ResetAction implements Action{
-  readonly type = DECREMENT
-}
-export class IncrementByArgAction implements Action{
-  readonly type = INCREMENTBYARG
-  constructor(public payload: number){}
-}
-
-export type Actions =
-  | IncrementAction
-  | DecrementAction
-  | ResetAction
-  | IncrementByArgAction
-
+import * as CounterActions from "../_actions/counter"
 
 export function counterReducer(state: number = 0, action: Action){
   switch (action.type){
-    case INCREMENT:
+    case CounterActions.INCREMENT:
       return state + 1;
-    case DECREMENT:
+    case CounterActions.DECREMENT:
       return state - 1;
-    case RESET:
+    case CounterActions.RESET:
       return 0;
-    case INCREMENTBYARG:
+    case CounterActions.INCREMENTBYARG:
       return state;
     default:
       return state;
--- ngrxApp/src/app/app.component.ts
+++ ngrxApp/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {Store} from "@ngrx/store"
 import {Observable} from "rxjs/Observable"
-import {INCREMENT, DECREMENT, RESET, INCREMENTBYARG} from "./_reducers/counter"
+import {INCREMENT, DECREMENT, RESET, INCREMENTBYARG, CounterActions} from "./_actions/counter"
 
 interface AppState {
   counter: number;
