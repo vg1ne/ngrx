4720d9f8ac883548d2abee326ce75614f8231fd9
vgoreiko <vgoreiko@deloitte.com>
2017-08-08 16:30:25 +0300

    added @ngrx/store-devtools

--- ngrxApp/src/app/app.module.ts
+++ ngrxApp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { StoreModule } from '@ngrx/store';
 // import { EffectsModule } from '@ngrx/effects';
-// import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import {counterReducer} from "./_reducers/counter"
 
 import { AppComponent } from './app.component';
@@ -14,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({counter: counterReducer})
+    StoreModule.forRoot({counter: counterReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 5
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
