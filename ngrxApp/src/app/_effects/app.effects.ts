f43e58b2bba43cf325b66a46446b09d44477670b
vg1ne <mbox696@gmail.com>
2017-08-13 19:53:34 +0300

    removed unused

--- ngrxApp/src/app/_effects/app.effects.ts
+++ ngrxApp/src/app/_effects/app.effects.ts
@@ -2,11 +2,8 @@ import {Actions, Effect} from "@ngrx/effects"
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import {Injectable} from "@angular/core";
-import {Observable} from "rxjs/Observable";
 import 'rxjs/Rx';
-import {ADD_BOOK, AddBook} from "../book/_actions/book.actions";
-import {AppStore} from "../app.component";
-import {Store} from "@ngrx/store";
+import {ADD_BOOK} from "../book/_actions/book.actions";
 import {SelectTab} from "../_actions/user.actions";
 
 @Injectable()
@@ -14,12 +11,11 @@ export class AppEffects {
   @Effect({dispatch: false}) selectTab$ = this.actions$
     .ofType(ADD_BOOK)
     .map(action => action['payload'])
-    .mergeMap(payload => {
+    .switchMap(payload => {
       return [
         new SelectTab(0)
       ]
     })
-  constructor(private actions$: Actions,
-              private appStore: Store<AppStore>){}
+  constructor(private actions$: Actions){}
 }
 
